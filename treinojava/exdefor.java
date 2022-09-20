47f3450149d013cd0410ba6582419bd7c0b0a16d
hgleo <hleomelo1@gmail.com>
2022-09-19 21:03:53 -0300

    commit init

--- treinojava/exdefor.java
+++ treinojava/exdefor.java
@@ -1,5 +1,11 @@
 package treinojava;
 
 public class exdefor {
+    public static void main(String[] args){
+        String alunos[] = { "Hugo","Gabriela", "Matheus"};
+            for (int i = 0; i < alunos.length; i++) {   
+                System.out.println("O aluno no `índicie x= " + i + " é " + alunos[i]); 
+    }    
     
+}
 }
