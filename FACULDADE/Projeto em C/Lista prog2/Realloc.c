0c3de75e20a2439f89b06d326705091ab4ebac0e
hgleo <hleomelo1@gmail.com>
2022-10-05 18:03:39 -0300

    commit

--- FACULDADE/Projeto em C/Lista prog2/Realloc.c
+++ FACULDADE/Projeto em C/Lista prog2/Realloc.c
@@ -20,68 +20,79 @@ o vetor
 4) IMPRIMIR_VETOR: imprime todos os elementos do vetor
 */
 
-void menu(int **V,int &tam,int *alocado){
-    //char opcao;
-    printf("O que deseja fazer:  |(I)nserir|(T)amanho|(E)spaço|Im(P)imir|(S)air\n");
-    scanf("%c", opcao); 
+void show(int* V, int tam){
 
-    /* O uso de Switch case foi pq eu acho q fica mais visual o codigo */
-
-    switch(opcao){
-        case 'I': Insere(); 
-        break;
-
-        case 'T': Tamanho();
-        break;
-
-        case 'E': Espaço();
-        break;
-
-        case 'P': Imprime();
-        break;
-
-        case 'S': return 0;
-        break;
+    printf("V =");
+    for(int i=0; i<tam; i++){
+        printf(" %d",V[i]);
     }
-
-    menu(V,tam,alocado);
-
+    printf("\n");
 }
 
-int Insere(int **V, int *tam, int *alocado,int numero){
+void put(int **V, int *tam, int *alocado, int x){
+
 
     if(*V==NULL){
         *V=(int*)malloc(1*sizeof(int));
-        (*V)[*tam]=numero;
+        (*V)[*tam]=x;
         *tam=1;
         *alocado=1;
-        return menu(V,tam,alocado);
+        return;
     }
     if(*tam<*alocado){
-        V[*tam]= numero;
+        (*V)[*tam]=x;
         (*tam)++;
-    }
-    else{
-        V=(int*)realloc(V,2*(*tam)*sizeof(int));
-        V[*tam]=numero;
-        (*tam)++;
+    }else{
+        *V=(int*)realloc(*V,2*(*tam)*sizeof(int));
+        (*V)[*tam]=x;
+        (*tam)++;        
         (*alocado)*=2;
     }
+}
+void whichTam(int *V, int *tam){
 
+    if (*V==NULL){
+        *tam = 0;
+    }
+    printf("\n tam: %d\n",*tam);
 }
 
+void menu(int **V, int *tam, int *alocado){
+    int op,x;
+
+    printf("Escolha uma opcao:\n");
+    printf("   0 - Encerrar\n");
+    printf("   1 - Inserir\n");
+    printf("   2 - Tamanho\n");
+    printf("   3 - Imprimir\n");
+    scanf("%d",&op);
+    switch(op){
+        case 0:
+            return;
+        case 1:
+            printf("Digite um numero para inserir: ");
+            scanf("%d",&x);
+            put(V,tam,alocado,x);
+            break;
+        case 2:
+            whichTam(*V,*tam);
+            break;
+        case 3:
+            show(*V, *tam);
+            break;
+    }
+    menu(V,tam,alocado);
+
+}
 
 
 int main(){
 
-    int *v,numero;
-    *V=*v;
-    v = (int*)malloc(numero*sizeof(int));
+    int alocado=0, tam=0;
+    int* V=NULL;
 
-    printf("Insira valor pro Vetor: ");
-    scanf("%d",&numero);
-    //char opcao;
-    menu(V,tam,alocado);
+    menu(&V,&tam,&alocado);
 
-    return 0;
+
+return 0;
 }
