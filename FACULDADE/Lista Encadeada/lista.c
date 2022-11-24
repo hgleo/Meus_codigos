b157a96075bcfc9e6858b5b90070083660aac9e1
hgleo <hleomelo1@gmail.com>
2022-11-24 20:45:35 -0300

    commit

--- FACULDADE/Lista Encadeada/lista.c
+++ FACULDADE/Lista Encadeada/lista.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 // Criação de uma lista, usando struct.
 
@@ -19,18 +20,19 @@ Lst *lstInsert(Lst* l, int i){
     Lst* novo = (Lst*)malloc(sizeof(Lst));
     novo->info = i;
     novo->next = l;
-    printf("Numero inserido com sucesso. \n");
+    printf("Número inserido com sucesso. \n");
     return novo;
 }
 
 // Função que mostra a minha lista.
 
-Lst *lstPrint(Lst* l){
+void lstPrint(Lst* l){
     Lst* p;
-    printf("Lista: |");
+    printf("Lista [");
     for(p = l; p != NULL; p = p->next){
-        printf(" %d |", p->info);
+        printf(" %d ", p->info);
     }
+    printf("]");
 }
 
 // Libera a memória alocada da lista.
@@ -38,32 +40,33 @@ Lst *lstPrint(Lst* l){
 void lstFree(Lst* l){
     Lst* p = l;
     while(p!= NULL){
-        Lst* t = p->next; // Guarda referência para prox elemento.
+        Lst* t = p->next; // Guarda referência para next elemento.
         free(p); // Libera a memória apontada por p.
-        p=t; // Faz p apontar pro prox.
+        p=t; // Faz p apontar pro next.
     }
 }
 
 // Função que busca algum elemento da minha lista.
 
 Lst* lstSearch(Lst* l, int s){
-    Lst* s;
-    for(s = l; s!= NULL; s = s->next){
-        return printf("Num encontrado!\n");
+    Lst* p;
+    for(p = l; p!= NULL; p = p->next){
+        if(p->info == s) 
+        return p;
     }
-    return printf("Num n encontrado!\n");
+    return NULL;
 }
 
 // Função que retira algum elemento da lista, caso exista.
 
-void lstDelete(Lst* l, int d){
+Lst* lstDelete(Lst* l, int d){
     Lst* ant = NULL; // Aponta pro anterior.
     Lst* p = l; // Aponta pra percorrer a lista.
 
     // Verificação se existe esse elemento.
     while(p != NULL && p-> info != d){
         ant = p;
-        p = p-> prox;
+        p = p-> next;
     }
     // Verifica se achou elemento
     if(p == NULL){
@@ -72,24 +75,26 @@ void lstDelete(Lst* l, int d){
     // Retira elemento. 
     if(ant == NULL){
         // Retira do início.
-        l = p->prox;
+        l = p->next;
     }
     else{
         // Retira elemento do meio da lista.
-        ant-> prox = p->prox;
+        ant-> next = p->next;
     }
     free(p);
     return l;
 }
 int main(){
+    setlocale(LC_ALL,"");
     int opc;
     
     Lst* l; // Declara a lista vazia,
     l = lstCreate(); // Inicia a lista vazia.
     
     do{
-        printf("\nInsira a opc adequada para o que deseja: \n");
-        printf("[1] para inserir na lista.\n[2] Para exibir a lista.\n[3] Para buscar um elemento.\n[4] Para Excluir um elemento.\n[0] Para finalizar a lista.\n");
+        printf("\nInsira a opção adequada para o que deseja: \n");
+        printf("[1] Para inserir na lista.\n[2] Para exibir a lista.\n[3] Para buscar um elemento.\n[4] Para Excluir um elemento.\n[0] Para finalizar a lista.\n");
+        printf("Opção desejada: ");
         scanf("%d", &opc);
 
         switch(opc){
@@ -102,6 +107,18 @@ int main(){
             case 2:
                 lstPrint(l);
             break;
+            case 3:
+                int s;
+                printf("Insira o num: ");
+                scanf("%d", &s);
+                l = lstSearch(l,s);
+            break;
+            case 4:
+                int d;
+                printf("Insira o num: ");
+                scanf("%d", &d);
+                l = lstDelete(l,d);
+            break;
             case 0:
                 lstFree(l);
                 printf ("Lista finalizada com sucesso!\n");
@@ -112,6 +129,7 @@ int main(){
 
 
             system("clear");
+            system("cls");
         }
     }while(opc != 0);
     return 0;
